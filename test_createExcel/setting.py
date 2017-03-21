3bdb799f2333ce68e13551b6239df2ea51fdfd8c
githubChenzy <Chenzy161119@163.com>
2017-03-21 22:58:37 +0800

    Changes to be committed: 	modified:   test_createExcel/setting.py

--- test_createExcel/setting.py
+++ test_createExcel/setting.py
@@ -1,6 +1,41 @@
 
 #-*- coding:utf-8 -*-
 
+import os
+import logging
+
+
+LOGGING = {
+	
+	'version': 1,
+	'formatters': {
+		'fmt': {
+			'datafmt': '%Y-%m-%d %H:%M:%S',
+			'format': '%(asctime)s %(levelname)s %(name)s %(message)s'
+		}
+	},
+	'handlers'; {
+		'console': {
+			'class': 'logging.StringHandler',
+			'level': 'logging.DEBUG',
+			'formatter': 'fmt',
+		},
+		'error':{
+			'class': 'logging.RotatingFileHandler',
+			'level': 'logging.DEBUG',
+			'encoding': 'utf-8',
+			'maxBytes': 1024 * 10,
+			'formatter': 'fmt',
+		},
+
+
+	},
+	'mylogger': {
+
+	}
+
+}
+
 
 if __name__ == '__main__':
 	print('github ')
