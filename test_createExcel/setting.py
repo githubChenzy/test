c57a7e308217c50c55512c07d3727ac370fedbd8
githubChenzy <Chenzy161119@163.com>
2017-03-22 23:55:09 +0800

    Changes to be committed: 	modified:   test_createExcel/CreateExcel.py 	new file:   test_createExcel/__pycache__/setting.cpython-35.pyc 	new file:   test_createExcel/db/mysql.py 	new file:   test_createExcel/log/all.log 	new file:   test_createExcel/log/error.log 	new file:   test_createExcel/log/info.log 	new file:   test_createExcel/report/1490197793.xlsx 	modified:   test_createExcel/setting.py 	new file:   test_createExcel/test.py 	new file:   "test_createExcel/\347\224\250\346\210\267\346\212\245\350\241\250\344\277\241\346\201\257.txt"

--- test_createExcel/setting.py
+++ test_createExcel/setting.py
@@ -3,39 +3,95 @@
 
 import os
 import logging
+import logging.config
 
+mysql_config = dict(
+	host='192.168.5.200',
+	user='root',
+	passwd='root',
+	post=3306,
+	db='mg_sdk_game',
+	charset='utf-8',
+)
+
+log_path = os.path.join(os.path.dirname(__file__), 'log')
+print(log_path)
+
+def create_log_dir(path):
+	if not os.path.isdir(path):
+		os.mkdir(path)
+
+def create_log_file(path, filename):
+	create_log_dir(path)
+	return os.path.join(path, filename)
 
 LOGGING = {
-	
-	'version': 1,
-	'formatters': {
+	'version': 1,  # 必须为 整型 1
+	'disable_existing_loggers': True,
+	'formatters': {   # 配置格式
+		'fmt_error': {
+			'datefmt': '%Y-%m-%d %H:%M:%S',
+			'format': '%(asctime)s %(message)s'
+		},
 		'fmt': {
-			'datafmt': '%Y-%m-%d %H:%M:%S',
-			'format': '%(asctime)s %(levelname)s %(name)s %(message)s'
+			# 'datefmt': '%Y-%m-%d %H:%M:%S',
+			'datefmt': '%a, %b %Y %m %d %H:%M:%S',
+			'format': '%(asctime)s [filename: %(filename)s] [line: %(lineno)s]: %(message)s'
 		}
 	},
-	'handlers'; {
+	'handlers': {  # 各种handler的一些配置参数
+		'null': {
+			'class': 'logging.NullHandler',  # handler的类型
+			'level': 'DEBUG'				# 日志等级
+		},
 		'console': {
-			'class': 'logging.StringHandler',
-			'level': 'logging.DEBUG',
-			'formatter': 'fmt',
+			'class': 'logging.StreamHandler',
+			'level': 'DEBUG',
+			'formatter': 'fmt'
 		},
-		'error':{
-			'class': 'logging.RotatingFileHandler',
-			'level': 'logging.DEBUG',
+		'errors': {
+			'class': 'logging.handlers.RotatingFileHandler',
+			'level': 'ERROR',
+			'filename': create_log_file(log_path, 'error.log'),
+			'formatter': 'fmt',
 			'encoding': 'utf-8',
-			'maxBytes': 1024 * 10,
-			'formatter': 'fmt',
+			'maxBytes': 1024 * 1024 * 10,
+			'backupCount': 5
 		},
-
-
+		'info': {
+			'class': 'logging.handlers.RotatingFileHandler',
+			'level': 'INFO',
+			'filename': create_log_file(log_path, 'info.log'),
+			'formatter': 'fmt',
+			'encoding': 'utf-8',
+			'maxBytes': 1024 * 1024 * 10,
+			'backupCount': 5
+		},
+		'notes': {
+			'class': 'logging.handlers.RotatingFileHandler',
+			'level': 'DEBUG',
+			'filename': create_log_file(log_path, 'all.log'),
+			'formatter': 'fmt',
+			'encoding': 'utf-8',
+			'maxBytes': 1024 * 1024 * 10,
+			'backupCount': 5
+		}
 	},
-	'mylogger': {
-
+	'loggers': {
+		'mylogger': {
+			'level': 'DEBUG',
+			'handlers': ['console', 'notes', 'info', 'errors'],
+			'propagate': True,
+		}
 	}
-
 }
 
 
+
 if __name__ == '__main__':
+	logging.config.dictConfig(LOGGING)
+	logger = logging.getLogger('mylogger')
+	logger.error("test logging error")
+	logger.info("test logging info")
+
 	print('github ')
